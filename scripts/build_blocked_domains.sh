2ce0c8f331afffbf4453f452cfbe7aa671833665
Skillmio <skillmiocfs@gmail.com>
2026-03-14 00:06:21 +0100

    Update build_blocked_domains.sh

--- scripts/build_blocked_domains.sh
+++ scripts/build_blocked_domains.sh
@@ -99,8 +99,8 @@ cd "$REPO_DIR"
 
 cp "$TMP_DIR/$FILE_TO_UPLOAD" "$REPO_DIR/"
 
-git config user.name "bot-updater"
-git config user.email "bot@skillmio.net"
+git config user.name "Skillmio"
+git config user.email "skillmiocfs@gmail.com"
 git add "$FILE_TO_UPLOAD"
 
 if ! git diff --cached --quiet; then
